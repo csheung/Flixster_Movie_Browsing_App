f6c54995a97c18f1f486e173afb30e5df821e0d1
Derrick Ng <99443055+csheung@users.noreply.github.com>
2023-03-03 13:49:30 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Derrick Ng on 3/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
